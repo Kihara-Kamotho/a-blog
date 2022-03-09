01ad43fa2a37abd298f161a10f12e78c2709855b
panyo <kiharaalphan@gmail.com>
2022-03-09 14:54:54 +0300

    create-post-spec

---
+++ spec/features/create_post_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "user can create a post" do
+  it ", with valid attributes" do
+    # visit root path, posts#index
+    visit "/"
+    # click link New post
+    click_link "New Post"
+    # fill_in new post
+    fill_in "Title", with: "Post one"
+    fill_in "Description", with: "Post one desc"
+
+    click_button "Create Post"
+  end
+end
