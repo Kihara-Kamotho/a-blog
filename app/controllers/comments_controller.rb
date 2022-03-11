2bec5d4a947f97fd9448f668fddbebb537975222
panyo <kiharaalphan@gmail.com>
2022-03-11 18:47:17 +0300

    delete-comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,11 +27,21 @@ class CommentsController < ApplicationController
   def update
     @comment = @post.comments.find(params[:id])
     if @comment.update(comment_params)
-      redirect_to post_comment_path(@post, @comment)
+      redirect_to post_path(@post)
       flash.now[:notice] = "Comment successfully updated"
     else
       render :edit
       flash.now[:alert] = "Comment not updated"
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      redirect_to post_comment_path(@post, @comment)
+      flash[:alert] = "Comment deleted"
+    else
+      puts "Cannot destroy comment"
     end
   end
 
---
+++ spec/features/delete_comment_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "user can delete comment" do
+  it ", with valid attributes" do
+    post = FactoryBot.create(:post, title: "Post one", description: "Post one desc")
+    FactoryBot.create(:comment, post: post, content: "Hello world")
+
+    # visit posts#show
+    visit post_path(post)
+    # click btn delete
+    click_link "Delete Comment"
+    # assertion
+    expect(page).to have_content "Comment has been deleted"
+  end
+end
