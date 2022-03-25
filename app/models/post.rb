cb02ec3dd769c607694a748b95226ba7701966ad
panyo <kiharaalphan@gmail.com>
2022-03-25 18:55:07 +0300

    styling app

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image
+  attr_reader :author
 
   def thumbnail
     return self.image.variant(resize_to_fill: [100, nil])
+  end
+
+  def author
+    self.title
   end
 end
