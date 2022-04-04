84f47b29815f4f96a74531e79301f2e2725a8f74
panyo <kiharaalphan@gmail.com>
2022-04-04 13:51:39 +0300

    admin can create users

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,19 @@ Rails.application.routes.draw do
   namespace :admin do
     # root route for admin
     root "application#index"
+    resources :posts, except: [:index, :show]
+    resources :users
   end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   # non-admin routes
   root "posts#index"
   resources :posts, only: [:index, :show] do
     resources :comments
   end
 
-  # admin routes
-  namespace :admin do
-    resources :posts, except: [:index, :show] do
-      resources :comments
-    end
-  end
 end
--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -4,5 +4,18 @@ RSpec.feature "admin user can create new users" do
   # create an admin\
   let(:admin) {FactoryBot.create(:user, :admin)}
 
-  
+  before do
+    login_as(admin)
+    visit "/"
+    click_link "Admin"
+    click_link "Users"
+    click_link "New User"
+  end
+
+  scenario "admin user can create new user" do
+    fill_in "Email", with: "new-admin-user@example.com"
+    fill_in "Password", with: "password"
+    click_button "Create User"
+    expect(page).to have_content "User has been created"
+  end
 end
